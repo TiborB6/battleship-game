750376637cd81cff1cee14444e11cf74161f7a5f
Tibor Balogh <btibor@gmx.at>
2023-07-05 19:10:15 +0200

    Just some config changes

--- webpack.config.js
+++ webpack.config.js
@@ -52,6 +52,14 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
     ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
 };
